c69dac88206b404099359f7d58b994345d5dacaa
Varko <cmendozasoberanis@gmail.com>
2021-03-25 20:45:55 -0600

    Adding logic to definitions

--- myfile.py
+++ myfile.py
@@ -1,14 +1,14 @@
 def add(x, y):
-    pass
+    return x + y
 
 def subtract(x, y):
-    pass
+    return x - y
 
 def multiply(x, y):
-    pass
+    return x * y
 
 def divide(x, y):
-    pass
+    return x / y
 
 def square(x):
-    pass
+    return x**x
